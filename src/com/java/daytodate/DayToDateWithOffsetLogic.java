3dbbfdb20a4f280d052e1a6387034c73b64ba011
Diptiman <john.rocks692@gmail.com>
2016-10-01 13:53:29 +0530

    Program to convert dat to the week of the day

--- src/com/java/daytodate/DayToDateWithOffsetLogic.java
+++ src/com/java/daytodate/DayToDateWithOffsetLogic.java
@@ -6,34 +6,43 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Description: Find out the day of the week based on the given date entry
+ * 
+ * @author Diptiman Adak
+ * Date: 10-01-2016
+ * 
+ */
 public class DayToDateWithOffsetLogic {
 
+	/* Map for the day number and day name of the week */
 	private static final HashMap<Integer, String> WEEKDAY_NUMBER = new HashMap<Integer, String>() {
 		{
 			put(0, "Sunday");
 			put(1, "Monday");
 			put(2, "Tuesday");
 			put(3, "Wednesday");
-			put(4, "Thrusday");
+			put(4, "Thursday");
 			put(5, "Friday");
 			put(6, "Saturday");
 		}
 	};
 
-	private static final HashMap<Integer, String> MONTH_OFFSET = new HashMap<Integer, String>() {
+	/* Mapping between month number and month offset */
+	private static final HashMap<Integer, Integer> MONTH_OFFSET = new HashMap<Integer, Integer>() {
 		{
-			put(0, "Jan");
-			put(3, "Feb");
-			put(3, "Mar");
-			put(6, "Apr");
-			put(1, "May");
-			put(4, "Jun");
-			put(6, "Jul");
-			put(2, "Aug");
-			put(5, "Sep");
-			put(0, "Oct");
-			put(3, "Nov");
-			put(5, "Dec");
+			put(1, 0);
+			put(2, 3);
+			put(3, 3);
+			put(4, 6);
+			put(5, 1);
+			put(6, 4);
+			put(7, 6);
+			put(8, 2);
+			put(9, 5);
+			put(10, 0);
+			put(11, 3);
+			put(12, 5);
 		}
 	};
 
@@ -41,24 +50,31 @@ public class DayToDateWithOffsetLogic {
 			IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
+
 		System.out.println("Enter the date in the number format(1-31):");
 		int date = Integer.parseInt(reader.readLine().trim());
 		System.out
-				.println("Enter the month with first three letters,e.g. Nov for November");
-		String month = reader.readLine().trim();
-		System.out.println("Enter the year in number format, e.g. 2009");
+				.println("Enter the month in the number format,e.g. 11 for November:");
+		int month = Integer.parseInt(reader.readLine().trim());
+		System.out.println("Enter the year in number format, e.g. 2009:");
 		int year = Integer.parseInt(reader.readLine().trim());
-		if(!validateInputDate(year, month, date))
+
+		// int year = 2016;
+		// int date = 1;
+		// String month = "Oct";
+		if (!validateInputDate(year, month, date)) {
 			System.out.println("Invalid Entry");
+			return;
+		}
 		int dayOffset = date % 7;
 		int yearOffset = (year % 100 + (year % 100) / 4) % 7;
-		if (checkLeapYear(year)) {
+		if (checkLeapYear(year) && (month == 1 || month == 2)) {
 			yearOffset = yearOffset - 1;
 		}
 		int monthOffset = 0;
-		for (Map.Entry<Integer, String> entryMonth : MONTH_OFFSET.entrySet()) {
-			if (entryMonth.getValue().equalsIgnoreCase(month))
-				monthOffset = entryMonth.getKey();
+		for (Map.Entry<Integer, Integer> entryMonth : MONTH_OFFSET.entrySet()) {
+			if (entryMonth.getKey() == month)
+				monthOffset = entryMonth.getValue();
 		}
 		DayToDateWithOffsetLogic dayToDateWithOffsetLogic = new DayToDateWithOffsetLogic();
 		int centuryOffset = dayToDateWithOffsetLogic.findCenturyOffset(year);
@@ -67,12 +83,24 @@ public class DayToDateWithOffsetLogic {
 				+ WEEKDAY_NUMBER.get(totalOffset % 7));
 	}
 
+	/**
+	 * Check if a given year is a leap year
+	 * 
+	 * @param year
+	 * @return true if leap year
+	 */
 	private static boolean checkLeapYear(int year) {
 		if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0))
 			return true;
 		return false;
 	}
 
+	/**
+	 * Calculate the century offset number
+	 * 
+	 * @param year
+	 * @return
+	 */
 	private static int countryOffsetCreation(int year) {
 		int firstTwoDigit = year / 100;
 		int nextHighestNumber = DayToDateWithOffsetLogic
@@ -80,6 +108,12 @@ public class DayToDateWithOffsetLogic {
 		return ((nextHighestNumber - 1) - firstTwoDigit) * 2;
 	}
 
+	/**
+	 * Find the next multiple of four for a given number
+	 * 
+	 * @param number
+	 * @return
+	 */
 	private static int findNextMultipleOfFour(int number) {
 		for (int i = number + 1; i <= 100; i++) {
 			if (i % 4 == 0) {
@@ -89,11 +123,23 @@ public class DayToDateWithOffsetLogic {
 		return 0;
 	}
 
+	/**
+	 * Find the century offset for the given year
+	 * 
+	 * @param year
+	 * @return centuryOffset
+	 */
 	private int findCenturyOffset(int year) {
 		return DayToDateWithOffsetLogic
 				.countryOffsetCreation(smallerNumberDivisibleByHundred(year));
 	}
 
+	/**
+	 * Find the immediate smaller number immediate to the given year
+	 * 
+	 * @param year
+	 * @return number
+	 */
 	private static int smallerNumberDivisibleByHundred(int year) {
 		for (int i = year; i >= 0; i--) {
 			if (i % 100 == 0)
@@ -102,19 +148,19 @@ public class DayToDateWithOffsetLogic {
 		return 0;
 	}
 
-	private static boolean validateInputDate(int year, String month, int date) {
-		if (year < 0 || year > 9999 || date < 0 || date > 31) {
+	/**
+	 * Validate the day,month and year as input
+	 * 
+	 * @param year
+	 * @param month
+	 * @param date
+	 * @return true if input is valid
+	 */
+	private static boolean validateInputDate(int year, int month, int date) {
+		if (year < 0 || year > 9999 || date < 0 || date > 31 || month < 0
+				|| month > 12) {
 			return false;
 		}
-		boolean monthFlag = false;
-		for (Map.Entry<Integer, String> entryMonth : MONTH_OFFSET.entrySet()) {
-			if (entryMonth.getValue().equalsIgnoreCase(month)) {
-				monthFlag = true;
-			}
-
-		}
-		if (!monthFlag)
-			return false;
 		return true;
 	}
 
